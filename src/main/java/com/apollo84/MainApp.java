fac91fb1fce3467d3da84974332630dbdbfb9d31
baiv84 <65267292+baiv84@users.noreply.github.com>
2025-01-31 17:28:31 +0700

    Реализовал паттерн стратегия

---
+++ src/main/java/com/apollo84/MainApp.java
@@ -0,0 +1,9 @@
+package com.apollo84;
+
+import com.apollo84.patterns.Strategy;
+
+public class MainApp {
+    public static void main(String[] args) {
+        Strategy.demonstrate();
+    }
+}
--- src/main/java/com/apollo84/patterns/MainApp.java
+++
@@ -1,7 +0,0 @@
-package com.apollo84.patterns;
-
-public class MainApp {
-    public static void main(String[] args) {
-        System.out.println("Design Patterns");
-    }
-}
