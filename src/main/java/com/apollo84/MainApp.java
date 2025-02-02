73438176fbaeaaee084d637d607b1f7ee1fcc26a
baiv84 <65267292+baiv84@users.noreply.github.com>
2025-02-02 23:25:32 +0700

    Сделал рефакторинг кода - ушел от статического метода + добавил интерфейс для демонстрации паттерна

--- src/main/java/com/apollo84/MainApp.java
+++ src/main/java/com/apollo84/MainApp.java
@@ -2,16 +2,16 @@ package com.apollo84;
 
 import java.util.List;
 import java.util.Arrays;
-import com.apollo84.patterns.Builder;
-import com.apollo84.patterns.ChainResponsibility;
-import com.apollo84.patterns.Demonstator;
-import com.apollo84.patterns.Strategy;
+
+import com.apollo84.patterns.*;
 
 public class MainApp {
     public static void main(String[] args) {
         List<Demonstator> patterns = Arrays.asList(new Strategy(),
                                                    new ChainResponsibility(),
-                                                   new Builder());
+                                                   new Builder(),
+                                                   new Proxy()
+        );
         patterns.forEach(Demonstator::demonstrate);
     }
 }
--- src/main/java/com/apollo84/patterns/Proxy.java
+++ src/main/java/com/apollo84/patterns/Proxy.java
@@ -1,4 +1,8 @@
 package com.apollo84.patterns;
 
-public class Proxy {
+public class Proxy implements Demonstator{
+    @Override
+    public void demonstrate() {
+        System.out.println("Proxy pattern demonstration");
+    }
 }
